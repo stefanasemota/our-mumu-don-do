6db35b3e602ee25b9495ea4712c623d18c367c69
Stefan Asemota <stephan.asemota@gmail.com>
2025-12-22 09:48:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-748727496-885ed",
+  "appId": "1:121047435612:web:02cdd96a283165c4c51999",
+  "apiKey": "AIzaSyAr552aMg_Aih9hJVaFfrEKEut8DiwzmWE",
+  "authDomain": "studio-748727496-885ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "121047435612"
+};
